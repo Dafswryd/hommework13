e7874ca24b0dccf21b116f951d6705d4cb06e1a4
Dafa Wira Yuda <dafwirayuda10@gmail.com>
2023-12-19 13:32:15 +0700

    fix: fix syntaks for UI

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,25 +1,32 @@
-import { VStack } from "@chakra-ui/react";
+import { Grid, GridItem, VStack } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import Books from "../components/Books";
 import { getAllBooks } from "../modules/fetch";
- 
-
 
 export default function Homepage() {
   const [books, setBooks] = useState([]);
   useEffect(() => {
     const fetchBooks = async () => {
-      const books = await getAllBooks();
-      setBooks(books);
+      const fetchedBooks = await getAllBooks();
+      setBooks(fetchedBooks);
     };
     fetchBooks();
   }, []);
 
   return (
-    <VStack w="100vw">
+    <Grid
+      templateColumns="repeat(3, 1fr)"
+      gap={6}
+      p={6}
+      w="100%"
+      maxW="1200px"
+      mx="auto"
+    >
       {books?.books?.map((book) => (
-        <Books key={`${book.id} ${book.title}`} {...book} />
+        <GridItem key={`${book.id} ${book.title}`}>
+          <Books {...book} />
+        </GridItem>
       ))}
-    </VStack>
+    </Grid>
   );
 }
